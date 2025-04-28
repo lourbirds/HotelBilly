56feda3defd3a3e3f1850c3279f05963c6107420
jeiyanet <jeiya5934@proton.me>
2025-04-28 11:20:15 +0800

    refactor: added proper constants

--- hotelbilly-file/src/main/java/com/hotelbilly/file/FileStorage.java
+++ hotelbilly-file/src/main/java/com/hotelbilly/file/FileStorage.java
@@ -8,11 +8,13 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class FileStorage {
+    final static String TRANSACTION_HISTORY_FILE_LOCATION = "./transaction_history.txt";
+
     public static void CheckFileExists() {
         // Check if there is a transaction_history file
         // If yes, do nothing
         // If no, create it
-        File txtFile = new File( "./transaction_history.txt");
+        File txtFile = new File(TRANSACTION_HISTORY_FILE_LOCATION);
         if(!txtFile.exists()){
             try{
                 if(txtFile.createNewFile()){
@@ -31,7 +33,7 @@ public class FileStorage {
         int latestID = 0;
         String[] parts;
         try {
-            List<String> lines = Files.readAllLines(Paths.get("./transaction_history.txt"));
+            List<String> lines = Files.readAllLines(Paths.get(TRANSACTION_HISTORY_FILE_LOCATION));
             for (String line : lines) {
                 if (line.startsWith("ID:")) {
                     parts = line.split(":");
@@ -45,7 +47,7 @@ public class FileStorage {
     }
 
     public static void AppendTransaction() {
-        try (FileWriter writer = new FileWriter("./transaction_history.txt", true)) {
+        try (FileWriter writer = new FileWriter(TRANSACTION_HISTORY_FILE_LOCATION, true)) {
             writer.write("ID: " + getTransactionID().getLast() + "\n");
             writer.write("Type: " + getRoomType().getLast() + "\n");
             writer.write("Occupancy: " + getRoomOcc().getLast() + "\n");
